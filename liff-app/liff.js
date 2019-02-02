cddce5a4fe2f6b18333b11af5551d7c8aae05bf7
勇太 松永 <m1m17r22@oit.ac.jp>
2019-02-02 14:55:01 +0900

    Change USER_SERVICE_UUID

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = '3177ea61-5e4b-46bd-b5df-4dca2e8c8209'; // LED, Button
+const USER_SERVICE_UUID         = '206f489f-193d-4a3a-96ef-74de45743f72'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
