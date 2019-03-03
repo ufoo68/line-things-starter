585be17599a0f7f4d72a4e168cdfd98874324619
teppei696 <teppei6969@gmail.com>
2019-03-03 10:28:25 +0900

    エラーを追加

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -291,7 +291,7 @@ function getStatus() {
         }
     }).fail(function(jqXHR, textStatus) {
         console.log("error occured");
-        $("#test").text(URI + ": error occured");
+        $("#test").text(URI + ": error occured " + textStatus);
     });
 }
 
